fa1bd8dd74d8272f037cbd6bf0175c321b592dcc
Stella Tigre <stella.cannefax@gmail.com>
2015-09-01 21:53:03 -0500

    adjust test writing style

--- tests.js
+++ tests.js
@@ -5,6 +5,8 @@ mocha.setup('bdd');
 
 describe('dat.gui interface', () => {
 
+    var root = gui.__folders;
+
     describe('assignLinks function', () => {
       it('should put 15 GIF links in each layer\'s gifLinkBuffer, given 45 links', () => {
 
@@ -25,7 +27,7 @@ describe('dat.gui interface', () => {
     it('should change the play speed of its assigned layer', () => {
         for (var n=0; n <= 2; n++) {
             // .__controllers[1] is opacity
-            gui.__folders["gif " + (n+1)].__controllers[1].setValue(1);
+            root["gif " + (n+1)].__controllers[1].setValue(1);
             assert.equal(1, frames[n].style.opacity);
         }
     });
@@ -35,7 +37,7 @@ describe('dat.gui interface', () => {
     it('should change the blend mode of its assigned layer', () => {
 	    for (var n=0; n <= 2; n++) {
             // .__controllers[2] is blendMode
-            gui.__folders["gif " + (n+1)].__controllers[2].setValue("screen");
+            root["gif " + (n+1)].__controllers[2].setValue("screen");
             assert.equal("screen", frames[n].style["mix-blend-mode"]);
         }
     });
@@ -45,7 +47,7 @@ describe('dat.gui interface', () => {
     it('should change the play speed of its assigned layer', () => {
         for (var n=0; n <= 2; n++) {
             // .__controllers[3] is playSpeed
-            gui.__folders["gif " + (n+1)].__controllers[3].setValue(2);
+            root["gif " + (n+1)].__controllers[3].setValue(2);
             assert.strictEqual(2, frames[n].speed);
         }
     });
@@ -55,13 +57,13 @@ describe('dat.gui interface', () => {
     it('should change the playback mode of the layer to ping-pong when true', () => {
 		for (var n=0; n <= 2; n++) {
 			// .__controllers[4] is ping-pong
-			gui.__folders["gif " + (n+1)].__controllers[4].setValue(true);
+			root["gif " + (n+1)].__controllers[4].setValue(true);
 			assert.equal("true", frames[n].getAttribute("ping-pong"));
 		}
     });
 	it('should change the playback mode of the layer to forward when false', () => {
 		for (var n=0; n <= 2; n++) {
-			gui.__folders["gif " + (n+1)].__controllers[4].setValue(false);
+			root["gif " + (n+1)].__controllers[4].setValue(false);
 			assert.equal(null, frames[n].getAttribute("ping-pong"));
 		}
     });
@@ -73,7 +75,7 @@ describe('dat.gui interface', () => {
         it('should change the saturation of its assigned layer', () => {
             for (var n=0; n <= 2; n++) {
                 // .__controllers[0] is saturation
-                gui.__folders["gif " + (n+1)].__folders.filters.__controllers[0].setValue(4);
+                root["gif " + (n+1)].__folders.filters.__controllers[0].setValue(4);
                 assert.strictEqual("4",
                     frames[n].style.webkitFilter.match("saturate\\(([0-9])\\)")[1]);
             }
@@ -83,7 +85,7 @@ describe('dat.gui interface', () => {
         it('should change the contrast of its assigned layer', () => {
             for (var n=0; n <= 2; n++) {
                 // .__controllers[1] is contrast
-                gui.__folders["gif " + (n+1)].__folders.filters.__controllers[1].setValue(2);
+                root["gif " + (n+1)].__folders.filters.__controllers[1].setValue(2);
                 assert.strictEqual("2",
                     frames[n].style.webkitFilter.match("contrast\\(([0-9])\\)")[1]);
             }
@@ -93,7 +95,7 @@ describe('dat.gui interface', () => {
         it('should change the brightness of its assigned layer', () => {
             for (var n=0; n <= 2; n++) {
                 // .__controllers[2] is brightness
-                gui.__folders["gif " + (n+1)].__folders.filters.__controllers[2].setValue(0.5);
+                root["gif " + (n+1)].__folders.filters.__controllers[2].setValue(0.5);
                 assert.strictEqual("0.5",
                     frames[n].style.webkitFilter.match("brightness\\(([0-9]\\.[0-9])")[1]);
             }
@@ -103,7 +105,7 @@ describe('dat.gui interface', () => {
         it('should change the hue rotation of its assigned layer', () => {
             for (var n=0; n <= 2; n++) {
                 // .__controllers[3] is hueRotate
-                gui.__folders["gif " + (n+1)].__folders.filters.__controllers[3].setValue(90)
+                root["gif " + (n+1)].__folders.filters.__controllers[3].setValue(90)
                 assert.strictEqual("90", frames[n].style.webkitFilter.match("hue-rotate\\(([0-9]{2})")[1])
             }
         });
@@ -113,7 +115,7 @@ describe('dat.gui interface', () => {
         it('should change the rotation mode / transform of its assigned layer, ' +
            'according the the predetermined values in flipEnum', () => {
             for (var n=0; n <= 2; n++) {
-                var flipSwitch = gui.__folders["gif " + (n+1)].__controllers[5];
+                var flipSwitch = root["gif " + (n+1)].__controllers[5];
 
                 flipSwitch.setValue("X")
                 assert.equal("rotate3d(1, 0, 0, 180deg)", frames[n].style.transform)
